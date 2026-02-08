498cbfbe0c61da706816fe497ccd706e70fcee5a
charantejreddy <charantejreddykondreddy@gmail.com>
2026-02-09 02:48:06 +0530

    Update frontend with live Render URL and update README

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-us1t.onrender.com/api" : "/api",
   withCredentials: true,
 });
+
+
+
 
